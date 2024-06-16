b588ea607b661f78ea74b6d67b932bede96e1201
MerLant <merlantpc@gmail.com>
2024-06-17 00:17:48 +0500

    Release

--- src/components/FinishLift.vue
+++ src/components/FinishLift.vue
@@ -1,6 +1,5 @@
 <script setup lang="ts">
 import { ref, computed } from 'vue'
-import FloorItem from './FloorItem.vue'
 import FloorButton from './FloorButton.vue'
 import LiftCradle from './LiftCradle.vue'
 import { TransitionGroup } from 'vue'
@@ -15,6 +14,8 @@ const currentFloor = ref(1)
 const queue = ref<number[]>([])
 const preQueue = ref<number[]>([])
 const isMoving = ref(false)
+const isJustStarted = ref(false)
+const isNextEnded = ref(false)
 
 function addFloor() {
 	if (!isAddDisabled.value) {
@@ -22,33 +23,79 @@ function addFloor() {
 	}
 }
 
+function getCurrentSpeed() {
+	if (isJustStarted.value || isNextEnded.value) {
+		return 1000
+	} else return 800
+}
+
 function removeFloor() {
-	if (!isRemoveDisabled.value) {
-		floors.value--
+	if (isRemoveDisabled.value) {
+		return
+	}
+
+	if (queue.value.includes(floors.value)) {
+		queue.value = queue.value.filter((floor) => floor !== floors.value)
+		processQueue()
 	}
 
 	if (floors.value < currentFloor.value) {
-		addToQueue(floors.value) // Исправлено: перемещение лифта на текущий верхний этаж
+		addToQueue(floors.value)
 	}
 
-	if (queue.value.includes(currentFloor.value)) {
-		queue.value = queue.value.filter((floor) => floor !== currentFloor.value) // Исправлено: удаление текущего этажа из очереди
-	}
+	floors.value--
 }
 
-function addToQueue(floor) {
-	if (queue.value.length === 0) {
+function addToQueue(floor: number) {
+	if (queue.value.includes(floor) || preQueue.value.includes(floor)) {
+		return
+	}
+
+	if ((queue.value.length === 0 && currentFloor.value === 1) || floor === 1) {
 		queue.value.push(floor)
-	} else if (!queue.value.includes(floor) || preQueueueue.value.includes(floor)) {
-		if (Math.max(queue.value) > floor) {
+	} else {
+		if (Math.max(...queue.value) > floor) {
 			queue.value.push(floor)
-			preQueue.value.sort((a, b) => b - a)
+			queue.value.sort((a, b) => b - a)
+		} else if (currentFloor.value > floor && queue.value[0] < floor) {
+			queue.value.unshift(floor)
+			queue.value.sort((a, b) => b - a)
 		} else {
 			preQueue.value.push(floor)
 			preQueue.value.sort((a, b) => b - a)
 		}
 	}
 	processQueue()
+}
+
+function moveStep() {
+	if (currentFloor.value === queue.value[0]) {
+		queue.value.shift()
+		isMoving.value = false
+		isNextEnded.value = false
+		setTimeout(() => {
+			processQueue()
+		}, 1000)
+		return
+	}
+
+	let direction = currentFloor.value < queue.value[0] ? 1 : -1
+
+	if (currentFloor.value + direction > floors.value || currentFloor.value + direction < 1) {
+		isMoving.value = false
+		isJustStarted.value = false
+		return
+	}
+
+	if (currentFloor.value + direction === queue.value[0]) {
+		isNextEnded.value = true
+	}
+
+	currentFloor.value += direction
+	setTimeout(() => {
+		isJustStarted.value = false
+		moveStep()
+	}, getCurrentSpeed())
 }
 
 function processQueue() {
@@ -71,52 +118,35 @@ function processQueue() {
 		}
 		return
 	}
-
 	isMoving.value = true
-	const nextFloor = queue.value[0]
-
-	function moveStep(current, target) {
-		if (current === target) {
-			queue.value.shift()
-			isMoving.value = false
-			setTimeout(() => {
-				processQueue()
-			}, 1000)
-			return
-		}
-
-		let direction = current < target ? 1 : -1
-		setTimeout(() => {
-			currentFloor.value += direction
-			moveStep(currentFloor.value, target)
-		}, 1000)
-	}
-
-	setTimeout(() => {
-		moveStep(currentFloor.value, nextFloor)
-	}, 300)
+	isJustStarted.value = true
+	moveStep()
 }
 </script>
 
 <template>
 	<div class="lift">
 		<div class="lift__shaft">
-			<TransitionGroup name="floor" tag="div" class="lift__floors">
+			<TransitionGroup name="lift__floor" tag="div" class="lift__floors">
 				<FloorButton v-for="floor in floors" :key="floor" @click="addToQueue(floor)">
 					{{ floor }}
 				</FloorButton>
 			</TransitionGroup>
 			<LiftCradle
 				:style="{
-					bottom: (currentFloor - 1) * 60 + 5 + 'px',
-					transition: isMoving ? '' : 'all 1.5s ease-out'
+					bottom: `${(currentFloor - 1) * 60 + 5}px`,
+					transition: isNextEnded
+						? 'all 1.5s ease-out'
+						: isJustStarted
+							? 'all 1.2s ease-in'
+							: ''
 				}"
 				class="lift__cradle"
 			>
 				{{ currentFloor }}
 			</LiftCradle>
 		</div>
-		<div class="lift__button">
+		<div class="lift__buttons">
 			<FloorButton :disabled="isRemoveDisabled" @click="removeFloor"> - </FloorButton>
 			<FloorButton :disabled="isAddDisabled" @click="addFloor"> + </FloorButton>
 		</div>
@@ -143,7 +173,7 @@ function processQueue() {
 	gap: 10px;
 }
 
-.lift__button {
+.lift__buttons {
 	border: 1px solid var(--color-border);
 	padding: 8px;
 	border-radius: 8px;
@@ -157,17 +187,19 @@ function processQueue() {
 	transition: bottom 1s linear;
 }
 
-.floor-enter-active,
-.floor-leave-active {
+.lift__floor-enter-active,
+.lift__floor-leave-active {
 	transition:
 		opacity 0.2s ease,
 		transform 0.2s ease;
 }
-.floor-enter-from {
+
+.lift__floor-enter-from {
 	opacity: 0;
 	transform: translateY(-30px);
 }
-.floor-leave-to {
+
+.lift__floor-leave-to {
 	opacity: 0;
 	transform: translateY(30px);
 }
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,47 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import { ref } from 'vue'
+import Lift from '../components/FinishLift.vue'
+import FloorButton from '../components/FloorButton.vue'
+
+const lifts = ref(1)
+const maxLifts = ref(3)
+
+function addLift() {
+	if (lifts.value > maxLifts.value) {
+		return
+	}
+
+	lifts.value++
+}
+function removeLift() {
+	if (lifts.value - 1 < 1) {
+		return
+	}
+
+	lifts.value--
+}
 </script>
 
 <template>
 	<main>
-		<TheWelcome />
+		<FloorButton @click="removeLift" :disabled="lifts - 1 < 1">-</FloorButton>
+		<div class="lift-block">
+			<Lift v-for="(lift, index) in lifts" :key="index" />
+		</div>
+		<FloorButton @click="addLift" :disabled="lifts > maxLifts">+</FloorButton>
 	</main>
 </template>
+<style scoped>
+main {
+	display: flex;
+	align-items: flex-end;
+	gap: 40px;
+}
+
+.lift-block {
+	display: flex;
+	flex-direction: row;
+	align-items: flex-end;
+	gap: 10px;
+}
+</style>
